7c3a2256af5910ce4f9c460349ed06c1b2082acb
1ongames <114791859+1ongames@users.noreply.github.com>
2024-11-13 16:11:37 +0900

    Update slion.ts

--- src/events/slion.ts
+++ src/events/slion.ts
@@ -3,10 +3,13 @@ import type {Client} from 'discord.js';
 export default function setup(client: Client) {
     client.on('messageCreate', async (message) => {
         if (message.content === '시론아 이프') {
-            await message.reply('머랭!');
+            await message.send('머랭!');
         }
         if (message.content === '시론아 머랭') {
-            await message.reply('쿠키!');
+            await message.send('쿠키!');
+        }
+        if (message.content === '시론아 크시') {
+            await message.send('팀 점점 세게 소속의 자동응답기 맞나요?');
         }
     });
 }
