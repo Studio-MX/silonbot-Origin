ea42ec860395b7a9d105c5274080a083d8b04ea8
1ongames <114791859+1ongames@users.noreply.github.com>
2024-11-13 16:38:40 +0900

    Update bot.config.ts

--- src/config/bot.config.ts
+++ src/config/bot.config.ts
@@ -1,7 +1,7 @@
 import type {BotConfig} from '../types';
 
 export const botConfig: BotConfig = {
-    name: '낚시봇',
-    prefix: '낚시야',
-    helpText: `https://github.com/Studio-MX/slionbot-Origin 에 이 프로젝트는 공개되어 있습니다.`,
+    name: '시론',
+    prefix: '시론아',
+    helpText: `https://github.com/Studio-MX/slionbot-Origin 에 이 프로젝트는 공개되어 있습니다.\n해당 봇은 MIT 라이선스를 사용합니다. 이용에 참고 부탁드립니다.`,
 };
